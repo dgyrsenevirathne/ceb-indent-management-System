de9b00b316ee5d7442a579abd58bbcc8fbd60bf2
dgyrsenevirathne <yasithsenevirathna01@gmail.com>
2025-01-06 11:15:48 +0530

    edit dashbord.js to add date patern when export to excell sheet

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -277,6 +277,12 @@ function deleteIndent(indentNo) {
 
 // Function to export indents to Excel
 function exportToExcel(data) {
+
+    // Format date before exporting
+    data.forEach((indent) => {
+        indent.Date = new Date(indent.Date).toISOString().split('T')[0];
+    });
+
     const workbook = XLSX.utils.book_new();
     const worksheet = XLSX.utils.json_to_sheet(data);
     XLSX.utils.book_append_sheet(workbook, worksheet, 'Indents');
